91a3d30dedb734e1fea4a1325cfc807c3039c735
Anshul Pradhan <anshulpradhan1@gmail.com>
2023-06-29 19:42:12 +0530

    First commit.
    This is my first commit

---
+++ js/custom.js
@@ -0,0 +1,81 @@
+// Small Screen Menu Toggle
+$(document).ready(function () {
+    $('.hamburger').click(function(){
+        $('.mob-header').addClass('open-state');
+    })
+});
+$(document).ready(function () {
+    $('.menu-close').click(function(){
+        $('.mob-header').removeClass('open-state');
+    })
+});
+
+// Exposure Carousel
+$('.exposure .owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav: false,
+    center: true,
+    autoplay: true,
+    autoplayTimeout: 7000,
+    autoplaySpeed: 7000,
+    autoplayHoverPause: false,
+    animateOut: 'fadeOut',
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:1
+        }
+    }
+});
+
+// Custom Cursor 
+var cursor = $(".cursor"),
+    follower = $(".cursor-follower");
+
+var posX = 0,
+    posY = 0;
+
+var mouseX = 0,
+    mouseY = 0;
+
+TweenMax.to({}, 0.016, {
+  repeat: -1,
+  onRepeat: function() {
+    posX += (mouseX - posX) / 9;
+    posY += (mouseY - posY) / 9;
+    
+    TweenMax.set(follower, {
+        css: {    
+        left: posX - 12,
+        top: posY - 12
+        }
+    });
+    
+    TweenMax.set(cursor, {
+        css: {    
+        left: mouseX,
+        top: mouseY
+        }
+    });
+  }
+});
+
+$(document).on("mousemove", function(e) {
+    mouseX = e.clientX;
+    mouseY = e.clientY;
+});
+
+$("a, button").on("mouseenter", function() {
+    cursor.addClass("active");
+    follower.addClass("active");
+});
+$("a, button").on("mouseleave", function() {
+    cursor.removeClass("active");
+    follower.removeClass("active");
+});
